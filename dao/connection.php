f95ebccdd9ba424238851aef00edaf1db4f8806f
Editon Carlos <62724433+Editon-dev@users.noreply.github.com>
2022-07-22 15:37:12 -0300

    Update connection.php

--- dao/connection.php
+++ dao/connection.php
@@ -4,10 +4,6 @@
     $host = 'localhost';
     $db = 'agenda';
 
-    /*$username = 'admin';
-    $pwd = '3t3rn001';
-    $host = 'stg-db.c4xhxsxl9nsa.us-east-2.rds.amazonaws.com';
-    $db = 'ouvidoria_ipmbrasilpm';*/
     try{
         $conn = new PDO('mysql:host='.$host.';dbname='.$db,$username, $pwd);
         $conn->exec('SET CHARACTER SET utf8');
